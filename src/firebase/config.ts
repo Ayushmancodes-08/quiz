c028aeb942e3756fad1e820810d97f4985c40550
Ayushman Patra <patraayushman590@gmail.com>
2025-11-09 06:06:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2319423145-218b4",
+  "appId": "1:165593041898:web:62e30c00e9ef8e51580093",
+  "apiKey": "AIzaSyDugQgQjbv6bS2Et3lgikThsszbRdNX3os",
+  "authDomain": "studio-2319423145-218b4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "165593041898"
+};
